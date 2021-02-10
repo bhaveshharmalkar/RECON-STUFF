6002951517712e5ee2a855cfaf608ff66c336c98
Bhavesh Harmalkar <78724413+root-bhavesh@users.noreply.github.com>
2021-02-10 17:44:17 +0530

    Add files via upload

---
+++ setup.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+sudo apt-get update 
+sudo apt-get install golang
+git clone https://github.com/tomnomnom/assetfinder.git
+git clone  https://github.com/tomnomnom/httprobe.git
+git clone  https://github.com/blechschmidt/massdns.git
+cd massdns
+make
+cd bin
+mv massdns /usr/local/bin
+cd ..
+cd ..
+cd assetfinder
+go build
+mv assetfinder /usr/local/bin
+cd ..
+mv assetfinder /root
+mv httprobe /root
+mv massdns /root
+
+
+
+
+
+
+
+
+
+
