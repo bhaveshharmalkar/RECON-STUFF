c2758f1f4ea416650d281ac160f8acdfdabf26e0
Bhavesh Harmalkar <78724413+root-bhavesh@users.noreply.github.com>
2021-02-17 13:21:14 +0530

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,9 +1,9 @@
-#!/bin/bash
 sudo apt-get update 
 sudo apt-get install golang
 git clone https://github.com/tomnomnom/assetfinder.git
 git clone  https://github.com/tomnomnom/httprobe.git
 git clone  https://github.com/blechschmidt/massdns.git
+git clone https://github.com/s0md3v/Breacher.git
 cd massdns
 make
 cd bin
@@ -17,12 +17,10 @@ cd ..
 mv assetfinder /root
 mv httprobe /root
 mv massdns /root
-
-
-
-
-
-
+cd Breacher
+chmod +x breacher.py
+cd..
+mv Breacher /root
 
 
 
